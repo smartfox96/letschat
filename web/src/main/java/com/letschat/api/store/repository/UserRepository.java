52fa4dc4c4e0fad966069fe8bed760402265ac88
Vignesh Iyer <vigneshiyer@Vigneshs-MacBook-Pro-2.local>
2024-07-14 15:23:47 +0530

    Removing duplicate classes

--- web/src/main/java/com/letschat/api/store/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.letschat.api.store.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.letschat.api.store.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
